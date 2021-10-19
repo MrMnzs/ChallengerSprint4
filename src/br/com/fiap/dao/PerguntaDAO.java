e5d1c559b7c0112f123bb7791058704c4f1dd966
toterafa <79848666+toterafa@users.noreply.github.com>
2021-10-19 12:52:31 -0300

    atualizacao5

---
+++ src/br/com/fiap/dao/PerguntaDAO.java
@@ -0,0 +1,92 @@
+package br.com.fiap.dao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import br.com.fiap.connection.ConnectionFactory;
+import br.com.fiap.model.Pergunta;
+
+
+public class PerguntaDAO {
+	
+	public void insert (Pergunta pergunta) throws SQLException {
+		
+		Connection conexao = new ConnectionFactory().getConnection();
+		
+		PreparedStatement stmt = conexao.prepareStatement(
+				"INSERT INTO T_APL_PERGUNTAS (id_perguntas, nr_perguntas, ds_perguntas) VALUES (?, ?, ?)");
+		
+		stmt.setInt(1,pergunta.getId());
+		stmt.setInt(2,pergunta.getNrPergunta());
+		stmt.setString(3, pergunta.getDsPergunta());		
+		
+		stmt.execute();
+		System.out.println("Insert executado");
+		stmt.close();
+		conexao.close();
+	}
+	
+	public void update (Pergunta pergunta) throws SQLException {
+		Connection conexao = new ConnectionFactory().getConnection();
+		PreparedStatement stmt = conexao.prepareStatement(
+				"UPDATE T_APL_PERGUNTAS SET id_resposta=?, id_quiz=?, nr_perguntas=?, ds_perguntas=? WHERE id_perguntas=?");
+
+		stmt.setInt(1, pergunta.getResposta().getId());
+		stmt.setInt(2, pergunta.getQuiz().getId());
+		stmt.setInt(3, pergunta.getNrPergunta());
+		stmt.setString(4, pergunta.getDsPergunta());
+		stmt.setInt(5, pergunta.getId());
+		
+				
+		stmt.execute();
+		
+		System.out.println("Update executado");
+
+		stmt.close();
+		conexao.close();
+	}
+	
+	public void delete (Pergunta pergunta) throws SQLException {
+		Connection conexao = new ConnectionFactory().getConnection();
+		PreparedStatement stmt = conexao.prepareStatement(
+				"DELETE FROM T_APL_PERGUNTAS WHERE id_perguntas=?");
+		
+		stmt.setInt(1, pergunta.getId());
+		stmt.execute();
+		
+		System.out.println("Delete executado");
+		stmt.close();
+		conexao.close();
+	}
+	
+	public ArrayList<Pergunta> getPerguntas() throws SQLException{
+		ArrayList<Pergunta> lista = new ArrayList<>();
+		
+		Connection conexao = new ConnectionFactory().getConnection();
+		
+		PreparedStatement stmt = conexao.prepareStatement(
+				"SELECT id_perguntas, nr_perguntas, ds_perguntas FROM T_APL_PERGUNTAS order by id_perguntas");
+		
+		stmt.execute();
+		
+		ResultSet rs = stmt.getResultSet();
+		
+		while(rs.next()) {
+			Pergunta pergunta = new Pergunta();
+			pergunta.setId(rs.getInt("id_perguntas"));							
+			pergunta.setNrPergunta(rs.getInt("nr_perguntas"));
+			pergunta.setDsPergunta(rs.getString("ds_perguntas"));		
+			lista.add(pergunta);
+		}
+		
+		System.out.println(lista);
+				
+		rs.close();
+		stmt.close();
+		conexao.close();
+		
+		return lista;
+	}
+	
+}
---
+++ src/br/com/fiap/test/TestePergunta.java
@@ -0,0 +1,58 @@
+package br.com.fiap.test;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import br.com.fiap.dao.PerguntaDAO;
+import br.com.fiap.model.Pergunta;
+import br.com.fiap.model.Resposta;
+
+
+public class TestePergunta {
+
+	public static void main(String[] args) throws SQLException {
+		
+		PerguntaDAO dao = new PerguntaDAO();
+		
+		Pergunta p1 = new Pergunta();
+		
+		Resposta r1 =  new Resposta();
+		
+		r1.setId(5);
+		r1.setNrResposta(22);
+		r1.setDsResposta("Banana");
+		
+		p1.setId(21);
+		p1.getResposta().setId(5);
+		p1.getQuiz().setId(15);
+		p1.setNrPergunta(21);
+		p1.setDsPergunta("EAe blz?");
+		
+		dao.insert(p1);
+	
+		p1.setId(1);
+//		dao.delete(p1);		
+		
+		
+//		p1.setResposta(r1.getId());
+//		p1.getQuiz().setId(8);
+//		p1.setNrPergunta(20);
+//		p1.setDsPergunta("VAAAAAAAAAAAAAAAAAI CARAAAAI");
+//		p1.setId(2);
+//		
+//		dao.update(p1);
+		
+		
+//				
+//		ArrayList<Pergunta> pergunta = dao.getPerguntas();
+//		
+//		for(Pergunta u : pergunta) {
+//			System.out.println(u.getId());
+//			System.out.println(u.getNrPergunta());
+//			System.out.println(u.getDsPergunta());
+//		}
+		
+		
+		
+	}
+
+}
